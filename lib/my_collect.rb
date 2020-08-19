f95b0dfa525c5a8954ccf6acb9a2dd864b679cd4
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 22:15:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 def my_collect(array)
-  collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
+  array = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(array) do |lang|
   lang.upcase
 end
 return my_collect
